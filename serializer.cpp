35c35b5bf23ac840132b335e78bbbc6eed694600
Frerich Raabe <raabe@froglogic.com>
2010-07-21 01:34:07 +0200

    Don't hardcode buffer sizes
    Furthermore, there are no UTF-8 issues here; the input strings are
    UTF-8 and the result will be UTF-8 as well.

--- serializer.cpp
+++ serializer.cpp
@@ -17,59 +17,51 @@ void PlaintextSerializer::setTimestampsShown( bool timestamps )
 
 vector<char> PlaintextSerializer::serialize( const TraceEntry &entry )
 {
-    char timestamp[64] = { '\0' };
+    ostringstream str;
 
     if ( m_showTimestamp ) {
+        char timestamp[64] = { '\0' };
         strftime(timestamp, sizeof(timestamp), "%d.%m.%Y %H:%M:%S: ", localtime(&entry.timeStamp));
+        str << timestamp;
     }
 
-    string variableDump;
+    str << entry.tracePoint->sourceFile << ":" << entry.tracePoint->lineno << ": " << entry.tracePoint->functionName;
+
     if ( entry.variables && !entry.variables->empty() ) {
-        variableDump = "; Variables: { ";
+        str << "; Variables: { ";
         vector<AbstractVariableConverter *>::const_iterator it, end = entry.variables->end();
         for ( it = entry.variables->begin(); it != end; ++it ) {
-            variableDump += ( *it )->name();
-            variableDump += "=";
-            // XXX Consider encoding issues; (*it)->toString() yields UTF-8)
-            variableDump += ( *it )->toString();
-            variableDump += " ";
+            str << ( *it )->name() << "=" << ( *it )->toString() << " ";
         }
-        variableDump += "}";
+        str << "}";
     }
 
-    string backtrace;
     if ( entry.backtrace ) {
-        ostringstream str;
         str << "; Backtrace: { ";
         for ( size_t i = 0; i  < entry.backtrace->depth(); ++i ) {
             const StackFrame &frame = entry.backtrace->frame( i );
-            // XXX Consider encoding issues; frame.module and frame.sourceFile
-            // yield UTF-8
             str << "#" << i << ": in " << frame.module <<
                                ": " << frame.function << "+0x" << hex << frame.functionOffset
                                << " (" << frame.sourceFile << ":" << frame.lineNumber << ") ";
         }
         str << "}";
-        backtrace = str.str();
     }
 
-    vector<char> buf( 16384, '\0' ); // XXX don't hardcode buffer size
+    const string result = str.str();
 
-    // XXX Consider encoding issues
-    _snprintf(&buf[0], buf.size(), "%s%s:%d: %s%s%s", timestamp, entry.tracePoint->sourceFile, entry.tracePoint->lineno, entry.tracePoint->functionName, variableDump.c_str(), backtrace.c_str() );
+    vector<char> buf( result.begin(), result.end() );
+    buf.push_back( '\0' );
     return buf;
 }
 
 vector<char> CSVSerializer::serialize( const TraceEntry &entry )
 {
     ostringstream str;
-    // XXX Consider encoding issues
     str << entry.tracePoint->verbosity << "," << escape( entry.tracePoint->sourceFile ) << "," << entry.tracePoint->lineno << "," << escape( entry.tracePoint->functionName );
 
     if ( entry.variables ) {
         vector<AbstractVariableConverter *>::const_iterator it, end = entry.variables->end();
         for ( it = entry.variables->begin(); it != end; ++it ) {
-            // XXX Consider encoding issues
             str << "," << escape( ( *it )->name() ) << "," << escape( ( *it )->toString() );
         }
     }
