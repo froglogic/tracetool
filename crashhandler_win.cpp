c5658e422d6930b2e4b59977df72e470fc39a4ee
Frerich Raabe <raabe@froglogic.com>
2010-07-23 18:00:04 +0200

    Let windows crashhandler log an ERROR message with bt

--- crashhandler_win.cpp
+++ crashhandler_win.cpp
@@ -1,18 +1,33 @@
 #include "tracelib_config.h"
+#include "backtrace.h"
+#include "tracelib.h"
+
+#include <string>
 
 #include <windows.h>
 
 static LPTOP_LEVEL_EXCEPTION_FILTER g_prevExceptionFilter = 0;
 
+TRACELIB_NAMESPACE_BEGIN
+
 static LONG WINAPI tracelibExceptionFilterProc( LPEXCEPTION_POINTERS ex )
 {
+    BacktraceGenerator backtraceGenerator;
+    Backtrace *bt = new Backtrace( backtraceGenerator.generate( 8 ) );
+
+    const StackFrame &f = bt->frame( 0 );
+
+    static TracePoint tp( TracePoint::ErrorPoint, 0,
+                          f.sourceFile.c_str(), f.lineNumber, f.function.c_str() );
+    TraceEntry te( &tp, "The application crashed at this point!" );
+    te.backtrace = bt;
+    getActiveTrace()->addEntry( te );
+
     if ( g_prevExceptionFilter ) {
         return g_prevExceptionFilter( ex );
     }
     return EXCEPTION_CONTINUE_SEARCH;
 }
-
-TRACELIB_NAMESPACE_BEGIN
 
 void installCrashHandler()
 {
