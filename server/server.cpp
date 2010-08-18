10bb00ccb234ff9686b792994dfbfc382a9cfdd0
Frerich Raabe <raabe@froglogic.com>
2010-08-18 12:50:15 +0200

    Store deserialized TraceEntry structs in SQL database

--- server/server.cpp
+++ server/server.cpp
@@ -3,8 +3,10 @@
 #include <QDomDocument>
 #include <QDebug>
 #include <QSqlDatabase>
+#include <QSqlQuery>
 #include <QTcpServer>
 #include <QTcpSocket>
+#include <QVariant>
 
 #include <cassert>
 
@@ -23,6 +25,61 @@ static TraceEntry deserializeTraceEntry( const QDomElement &e )
     entry.function = e.namedItem( "function" ).toElement().text();
     entry.message = e.namedItem( "message" ).toElement().text();
     return entry;
+}
+
+static void storeEntryInDatabase( const TraceEntry &e, QSqlDatabase *db )
+{
+    QSqlQuery query;
+    query.setForwardOnly( true );
+
+    query.exec( "BEGIN TRANSACTION;" );
+
+    unsigned int pathId;
+    bool ok;
+    {
+        query.exec( QString( "SELECT id FROM path_name WHERE name='%1';" ).arg( e.path ) );
+        if ( !query.next() ) {
+            query.exec( QString( "INSERT INTO path_name VALUES(NULL, '%1');" ).arg( e.path ) );
+            query.exec( "SELECT last_insert_rowid() FROM path_name LIMIT 1;" );
+            query.next();
+        }
+        pathId = query.value( 0 ).toUInt( &ok );
+        if ( !ok ) {
+            qWarning() << "Database corrupt? Got non-numeric integer field";
+        }
+    }
+
+    unsigned int functionId;
+    {
+        query.exec( QString( "SELECT id FROM function_name WHERE name='%1';" ).arg( e.function ) );
+        if ( !query.next() ) {
+            query.exec( QString( "INSERT INTO function_name VALUES(NULL, '%1');" ).arg( e.function ) );
+            query.exec( "SELECT last_insert_rowid() FROM function_name LIMIT 1;" );
+            query.next();
+        }
+        functionId = query.value( 0 ).toUInt( &ok );
+        if ( !ok ) {
+            qWarning() << "Database corrupt? Got non-numeric integer field";
+        }
+    }
+
+    unsigned int tracepointId;
+    {
+        query.exec( QString( "SELECT id FROM trace_point WHERE verbosity=%1 AND type=%2 AND path_id=%3 AND line=%4 AND function_id=%5;" ).arg( e.verbosity ).arg( e.type ).arg( pathId ).arg( e.lineno ).arg( functionId ) );
+        if ( !query.next() ) {
+            query.exec( QString( "INSERT OR IGNORE INTO trace_point VALUES(NULL, %1, %2, %3, %4, %5);" ).arg( e.verbosity ).arg( e.type ).arg( pathId ).arg( e.lineno ).arg( functionId ) );
+            query.exec( "SELECT last_insert_rowid() FROM trace_point LIMIT 1;" );
+            query.next();
+        }
+        tracepointId = query.value( 0 ).toUInt( &ok );
+        if ( !ok ) {
+            qWarning() << "Database corrupt? Got non-numeric integer field";
+        }
+    }
+
+    query.exec( QString( "INSERT INTO trace_entry VALUES(NULL, %1, %2, %3, %4, '%5');" ).arg( e.pid ).arg( e.tid ).arg( e.timestamp ).arg( tracepointId ).arg( e.message ) );
+
+    query.exec( "COMMIT;" );
 }
 
 Server::Server( QObject *parent, QSqlDatabase *db, unsigned short port )
@@ -47,13 +104,16 @@ void Server::handleIncomingData()
 {
     QTcpSocket *client = (QTcpSocket *)sender(); // XXX yuck
 
+    const QByteArray xmlData = client->readAll();
+
     QDomDocument doc;
-    if ( !doc.setContent( client->readAll(), false ) ) {
+    if ( !doc.setContent( xmlData ) ) {
         qWarning() << "Error in incoming XML data";
         return;
     }
 
     const TraceEntry e = deserializeTraceEntry( doc.documentElement() );
+    storeEntryInDatabase( e, m_db );
     emit traceEntryReceived( e );
 }
 
