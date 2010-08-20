4ea0cd6d60531a32cfb8f82e92c4c98729204e84
Koos Vriezen <vriezen@froglogic.com>
2010-08-20 17:21:40 +0200

    stderr for unix error log

--- core/errorlog.cpp
+++ core/errorlog.cpp
@@ -2,6 +2,8 @@
 
 #ifdef _WIN32
 #  include <windows.h>
+#else
+#  include <stdio.h>
 #endif
 
 using namespace std;
@@ -21,6 +23,8 @@ void DebugViewErrorLog::write( const string &msg )
     // XXX Consider encoding issues (msg is UTF-8).
 #ifdef _WIN32
     OutputDebugStringA( msg.c_str() );
+#else
+    fprintf( stderr, "%s\n", msg.c_str() );
 #endif
 }
 
