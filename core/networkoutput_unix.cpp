7a6878e9f8d2c7b95a40caef72b55fad1e45a82a
Koos Vriezen <vriezen@froglogic.com>
2010-08-20 15:06:35 +0200

    Implement unix networkoutput
    Agreed it to assume the caller does the blocking

--- core/networkoutput_unix.cpp
+++ core/networkoutput_unix.cpp
@@ -1,28 +1,80 @@
 #include "output.h"
 #include "errorlog.h"
 
+#include <string.h>
+#include <unistd.h>
 #include <assert.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 using namespace std;
 
 TRACELIB_NAMESPACE_BEGIN
 
-NetworkOutput::NetworkOutput( ErrorLog *errorLog, const string &remoteHost, unsigned short remotePort )
+static int connectTo( const string host, unsigned short port, ErrorLog *log )
 {
-    // XXX implement
-    assert( !"NetworkOutput class not implemented Unix" );
+    struct hostent *he = gethostbyname( host.c_str() );
+    if ( !he ) {
+        log->write( "connect: host '%s' not found\n", host.c_str() );
+        return -1;
+    }
+
+    struct sockaddr_in server;
+    int sock = socket( AF_INET, SOCK_STREAM, 0 );
+    server.sin_family = AF_INET;
+    memcpy( &server.sin_addr.s_addr, he->h_addr, he->h_length );
+    server.sin_port = htons( port );
+    if ( connect( sock, (const sockaddr *)&server, sizeof ( server ) ) )
+        return sock;
+
+    log->write( "connect: %s\n", strerror( errno ) );
+    return -1;
 }
+
+static int writeTo( int fd, const char *data, int length, ErrorLog *log )
+{
+    int nr;
+    do {
+        nr = write( fd, data, length );
+        if ( nr >= 0 || errno != EINTR )
+            break;
+    } while ( 1 );
+
+    if ( nr < length )
+        log->write( "write: %s\n", strerror( errno ) );
+    return nr;
+}
+
+NetworkOutput::NetworkOutput( ErrorLog *log, const string &host, unsigned short port )
+    : m_host( host ), m_port( port ), m_socket( -1 ), m_error_log( log )
+{}
 
 NetworkOutput::~NetworkOutput()
 {
 }
 
+bool NetworkOutput::open()
+{
+    if ( m_socket == -1 )
+        m_socket = connectTo( m_host, m_port, m_error_log );
+    return m_socket != -1;
+}
+
 bool NetworkOutput::canWrite() const
 {
+    return m_socket != -1;
 }
 
 void NetworkOutput::write( const vector<char> &data )
 {
+    if ( m_socket != -1 ) {
+        if ( writeTo( m_socket, &data[0], data.size(), m_error_log ) < data.size() ) {
+            ::close( m_socket );
+            m_socket = 1;
+        }
+    }
 }
 
 TRACELIB_NAMESPACE_END
--- core/networkoutput_unix.h
+++ core/networkoutput_unix.h
@@ -9,10 +9,16 @@ class ErrorLog;
 
 class NetworkOutput : public Output
 {
+    std::string m_host;
+    unsigned short m_port;
+    int m_socket;
+    ErrorLog *m_error_log;
+
 public:
     NetworkOutput( ErrorLog *errorLog, const std::string &remoteHost, unsigned short remotePort );
     virtual ~NetworkOutput();
 
+    virtual bool open();
     virtual bool canWrite() const;
     virtual void write( const std::vector<char> &data );
 };
