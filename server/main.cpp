8c9804efe5fd7195d95bd02b65292212e2567a9f
Harri Porten <porten@froglogic.com>
2010-08-30 16:17:59 +0200

    Let the server take the trace file as command line argument.

--- server/main.cpp
+++ server/main.cpp
@@ -1,12 +1,62 @@
 #include "server.h"
 
+#include "../convertdb/getopt.h"
+
 #include <QCoreApplication>
+#include <QFile>
+
+#include <iostream>
+#include <string>
+
+namespace Error
+{
+    const int None = 0;
+    const int CommandLineArgs = 1;
+    const int Open = 2;
+    const int Conversion = 3;
+};
+
+using namespace std;
+
+static void printUsage(const string &app)
+{
+    cout << "Usage: " << app << " --help" << endl
+         << "       " << app << " <.trace-file>" << endl;
+}
 
 int main( int argc, char **argv )
 {
     QCoreApplication app( argc, argv );
 
-    Server server( "tracedata.dat", 12382 );
+    GetOpt opt;
+    bool help;
+    QString traceFile;
+    opt.addSwitch("help", &help);
+    opt.addOptionalArgument("trace_file", &traceFile);
+    if (!opt.parse()) {
+	cout << "Invalid command line argument. Try --help." << endl;
+	return Error::CommandLineArgs;
+    }
+
+    if (help) {
+	printUsage(argv[0]);
+	return Error::None;
+    }
+    if (traceFile.isEmpty()) {
+	cout << "Missing trace file argument. Try --help." << endl;
+	return Error::CommandLineArgs;
+    }
+    if (!traceFile.endsWith(".trace", Qt::CaseInsensitive)) {
+	// ### Too picky? No real technical reason so far. But
+	// ### helps the IDE and who knows what else in the
+	// ### future. Click a file type association that allows
+	// ### opening traces by double-click.
+	cout << "Trace file is expected to have a "
+	     << ".trace suffix. " << endl;
+	return Error::CommandLineArgs;
+    }
+
+    Server server(traceFile, 12382);
 
     return app.exec();
 }
