fd07408c2c66d707dabc5daed6a3c2d92dc995c2
Frerich Raabe <raabe@froglogic.com>
2010-09-10 17:01:10 +0200

    Moved TracePoint and TracePointType into dedicated tracepoint.h
    filter.cpp can include this now instead of tracelib.h so that it becomes
    independant of anything which tracelib.h includes (like variabledumping.*).

---
+++ hooklib/tracepoint.h
@@ -0,0 +1,71 @@
+#ifndef TRACELIB_TRACEPOINT_H
+#define TRACELIB_TRACEPOINT_H
+
+#include "tracelib_config.h"
+#include "dlldefs.h"
+
+TRACELIB_NAMESPACE_BEGIN
+
+struct TracePointType {
+    enum TRACELIB_EXPORT Value {
+        None = 0
+#define TRACELIB_TRACEPOINTTYPE(name) ,name
+#include "tracepointtypes.def"
+#undef TRACELIB_TRACEPOINTTYPE
+    };
+
+    static const int *values() {
+        static const int a[] = {
+            None,
+#define TRACELIB_TRACEPOINTTYPE(name) name,
+#include "tracepointtypes.def"
+#undef TRACELIB_TRACEPOINTTYPE
+            -1
+        };
+        return a;
+    }
+
+    static const char *valueAsString( Value v ) {
+#define TRACELIB_TRACEPOINTTYPE(name) static const char str_##name[] = #name;
+#include "tracepointtypes.def"
+#undef TRACELIB_TRACEPOINTTYPE
+        switch ( v ) {
+            case None: return "None";
+#define TRACELIB_TRACEPOINTTYPE(name) case name: return str_##name;
+#include "tracepointtypes.def"
+#undef TRACELIB_TRACEPOINTTYPE
+        }
+        return 0;
+    }
+};
+
+class Configuration;
+
+struct TracePoint {
+    TRACELIB_EXPORT TracePoint( TracePointType::Value type_, unsigned short verbosity_, const char *sourceFile_, unsigned int lineno_, const char *functionName_ )
+        : type( type_ ),
+        verbosity( verbosity_ ),
+        sourceFile( sourceFile_ ),
+        lineno( lineno_ ),
+        functionName( functionName_ ),
+        lastUsedConfiguration( 0 ),
+        active( false ),
+        backtracesEnabled( false ),
+        variableSnapshotEnabled( false )
+    {
+    }
+
+    const TracePointType::Value type;
+    const unsigned short verbosity;
+    const char * const sourceFile;
+    const unsigned int lineno;
+    const char * const functionName;
+    const Configuration *lastUsedConfiguration;
+    bool active;
+    bool backtracesEnabled;
+    bool variableSnapshotEnabled;
+};
+
+TRACELIB_NAMESPACE_END
+
+#endif // !defined(TRACELIB_TRACEPOINT_H)
