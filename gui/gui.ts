23a631ebb97d515a5c043d7266000b885b9ea782
Andreas Pakulat <apaku@gmx.de>
2010-09-16 20:52:07 +0200

    Support translation of the codebase.
    This adds the initially generated .ts files and also adds cmake macro-calls
    to update them on each build and generate the .qm files. Now we just need
    to load them too...

---
+++ gui/gui.ts
@@ -0,0 +1,257 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>EntryItemModel</name>
+    <message>
+        <location filename="entryitemmodel.cpp" line="66"/>
+        <source>Missing required %1 driver.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="entryitemmodel.cpp" line="197"/>
+        <source>Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="entryitemmodel.cpp" line="199"/>
+        <source>Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="entryitemmodel.cpp" line="201"/>
+        <source>PID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="entryitemmodel.cpp" line="203"/>
+        <source>Thread</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="entryitemmodel.cpp" line="205"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="entryitemmodel.cpp" line="207"/>
+        <source>Line</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="entryitemmodel.cpp" line="209"/>
+        <source>Function</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="entryitemmodel.cpp" line="211"/>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="entryitemmodel.cpp" line="214"/>
+        <source>Verbosity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="entryitemmodel.cpp" line="217"/>
+        <source>Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FilterForm</name>
+    <message>
+        <location filename="filterform.ui" line="14"/>
+        <source>View Filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="filterform.ui" line="22"/>
+        <source>Application:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="filterform.ui" line="32"/>
+        <source>Process-Id:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="filterform.ui" line="39"/>
+        <source>Thread-Id:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="filterform.ui" line="46"/>
+        <source>Class/Function:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="filterform.ui" line="53"/>
+        <source>Message:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="filterform.ui" line="60"/>
+        <source>Type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>App Stalker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="28"/>
+        <source>Freeze</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="41"/>
+        <source>Filter...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="64"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="78"/>
+        <source>Trace Points</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="105"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="111"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="122"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="125"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="130"/>
+        <source>&amp;Open Trace...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="133"/>
+        <source>Ctrl+O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="138"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="30"/>
+        <source>Watch Points</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="110"/>
+        <source>Open Trace</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="112"/>
+        <source>Trace Files (*.trace)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="118"/>
+        <source>Open Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="119"/>
+        <source>Error opening trace file: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="126"/>
+        <source>About %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="127"/>
+        <source>&lt;qt&gt;Copyright %1 froglogic GmbH
+&lt;br&gt;&lt;br&gt;Built with Qt %2 on %3.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../server/database.cpp" line="105"/>
+        <source>Database version mismatch. Expected %1, found %2.
+You can run a conversion tool to upgrade and downgrade the database, respectively.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../server/database.cpp" line="139"/>
+        <location filename="../server/database.cpp" line="150"/>
+        <source>Failed to execute &apos;%1&apos;: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../server/database.cpp" line="168"/>
+        <source>Missing required %1 driver.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../server/database.cpp" line="188"/>
+        <source>Database %1 not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../server/database.cpp" line="239"/>
+        <source>Database already of right version.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../server/database.cpp" line="243"/>
+        <source>Database older than ours.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../server/database.cpp" line="289"/>
+        <source>Unhandled upgrade step</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../server/database.cpp" line="300"/>
+        <source>Database already up to date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../server/database.cpp" line="304"/>
+        <source>Database already of higher version.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WatchTree</name>
+    <message>
+        <location filename="watchtree.cpp" line="52"/>
+        <source>Missing required %1 driver.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
