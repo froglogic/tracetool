0c7af2d65d131b3f703d03bd40e19053d63e706d
Frerich Raabe <raabe@froglogic.com>
2010-10-15 12:59:51 +0200

    Make default convertVariable() specializations for primitive types inline
    By now it becomes clear that the convertVariable() specializations for
    basic C++ types are all very small. We can thus safely make them inline. This
    greatly reduces the numbers of symbols which we need to export from
    tracelib.dll and it makes it easier to extend this list in the future (since
    the list is not duplicated in the header file and in the .cpp file anymore).

--- hooklib/variabledumping.cpp
+++ hooklib/variabledumping.cpp
@@ -6,7 +6,6 @@
 #include "variabledumping.h"
 
 #include <cassert>
-#include <sstream>
 
 using namespace std;
 
@@ -101,50 +100,6 @@ VariableSnapshot &operator<<( VariableSnapshot &snapshot, AbstractVariable *v )
     snapshot.push_back( v );
     return snapshot;
 }
-
-#define TRACELIB_SPECIALIZE_CONVERSION(T, factoryFn) \
-template <> \
-VariableValue convertVariable( T val ) { \
-    return VariableValue::factoryFn( val ); \
-}
-
-template <typename T>
-string stringFromStringStream( const T &val ) {
-    ostringstream str;
-    str << val;
-    return str.str();
-}
-
-#define TRACELIB_SPECIALIZE_CONVERSION_USING_SSTREAM(T) \
-template <> \
-VariableValue convertVariable( T val ) { \
-    return VariableValue::stringValue( stringFromStringStream( val ) ); \
-}
-
-TRACELIB_SPECIALIZE_CONVERSION(bool, booleanValue)
-TRACELIB_SPECIALIZE_CONVERSION(short, numberValue)
-TRACELIB_SPECIALIZE_CONVERSION(unsigned short, numberValue)
-TRACELIB_SPECIALIZE_CONVERSION(int, numberValue)
-TRACELIB_SPECIALIZE_CONVERSION(unsigned int, numberValue)
-TRACELIB_SPECIALIZE_CONVERSION(long, numberValue)
-TRACELIB_SPECIALIZE_CONVERSION(unsigned long, numberValue)
-TRACELIB_SPECIALIZE_CONVERSION(float, floatValue)
-TRACELIB_SPECIALIZE_CONVERSION(double, floatValue)
-TRACELIB_SPECIALIZE_CONVERSION(long double, floatValue)
-TRACELIB_SPECIALIZE_CONVERSION_USING_SSTREAM(void *)
-TRACELIB_SPECIALIZE_CONVERSION_USING_SSTREAM(const void *)
-TRACELIB_SPECIALIZE_CONVERSION_USING_SSTREAM(char)
-TRACELIB_SPECIALIZE_CONVERSION_USING_SSTREAM(signed char)
-TRACELIB_SPECIALIZE_CONVERSION_USING_SSTREAM(unsigned char)
-TRACELIB_SPECIALIZE_CONVERSION_USING_SSTREAM(char *)
-TRACELIB_SPECIALIZE_CONVERSION_USING_SSTREAM(signed char *)
-TRACELIB_SPECIALIZE_CONVERSION_USING_SSTREAM(unsigned char *)
-TRACELIB_SPECIALIZE_CONVERSION_USING_SSTREAM(const char *)
-TRACELIB_SPECIALIZE_CONVERSION_USING_SSTREAM(const signed char *)
-TRACELIB_SPECIALIZE_CONVERSION_USING_SSTREAM(const unsigned char *)
-TRACELIB_SPECIALIZE_CONVERSION_USING_SSTREAM(std::string)
-
-#undef TRACELIB_SPECIALIZE_CONVERSION_USING_SSTREAM
 
 TRACELIB_NAMESPACE_END
 
