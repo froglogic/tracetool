c4718514546420250f18c3f659d5c4fbb279ef8d
Harri Porten <porten@froglogic.com>
2010-11-08 14:52:53 +0100

    Removed Squish-specific code.

--- examples/addressbook/main.cpp
+++ examples/addressbook/main.cpp
@@ -10,22 +10,10 @@
 
 #include "mainwindow.h"
 #include <QtGui/QApplication>
-#ifdef Q_WS_X11
-#include <QtGui/QWindowsStyle>
-#endif
-#ifdef Q_OS_AIX
-#include "../../../include/qtbuiltinhook.h"
-#endif
 
 int main(int argc, char *argv[])
 {
-#ifdef Q_WS_X11
-    QApplication::setStyle(new QWindowsStyle); // Avoid Gtk problems
-#endif
     QApplication app(argc, argv);
-#ifdef Q_OS_AIX
-    Squish::installBuiltinHook(); // On AIX we must manually install a hook
-#endif
     MainWindow mainWindow;
     mainWindow.resize(640, 480);
     mainWindow.show();
