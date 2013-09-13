1f6be95c061d60ffae6e89811a9a71cd01bca9e7
Andreas Pakulat <andreas@froglogic.com>
2013-09-13 16:08:56 +0200

    Make variabledumping_qt.h Qt3 compatible
    This change adapts the header so it can be used with Qt3 projects
    transparently. You can simply include it and the same Qt types as for
    Qt4 can be serialized in a Qt3 application.

--- hooklib/variabledumping_qt.h
+++ hooklib/variabledumping_qt.h
@@ -9,14 +9,25 @@
 #include "tracelib_config.h"
 #include "variabledumping.h"
 
-#include <QByteArray>
-#include <QChar>
-#include <QDate>
-#include <QDateTime>
-#include <QString>
-#include <QStringList>
-#include <QTime>
-#include <QVariant>
+#include <qglobal.h>
+
+#if QT_VERSION < 0x040000
+#  include <qcstring.h>
+#  include <qdatetime.h>
+#  include <qstring.h>
+#  include <qstringlist.h>
+#  include <qvariant.h>
+#else
+#  include <QByteArray>
+#  include <QChar>
+#  include <QDate>
+#  include <QDateTime>
+#  include <QString>
+#  include <QStringList>
+#  include <QTime>
+#  include <QVariant>
+
+#endif
 
 TRACELIB_NAMESPACE_BEGIN
 
@@ -26,25 +37,42 @@ inline VariableValue convertVariable( T val ) { \
     return VariableValue::factoryFn( val ); \
 }
 
+#if QT_VERSION >= 0x040000
 TRACELIB_SPECIALIZE_CONVERSION(qlonglong, numberValue)
 TRACELIB_SPECIALIZE_CONVERSION(qulonglong, numberValue)
+#endif
 
 #undef TRACELIB_SPECIALIZE_CONVERSION
+
+#if QT_VERSION < 0x040000
+#  define qtTypeToUtf8Char( value ) QVariant( value ).toString().utf8()
+#else
+#  define qtTypeToUtf8Char( value ) QVariant::fromValue( value ).toString().toUtf8().data()
+#endif
 
 #define TRACELIB_SPECIALIZE_CONVERSION_USING_QVARIANT(T) \
 template <> \
 inline VariableValue convertVariable( T val ) { \
-    return VariableValue::stringValue( QVariant::fromValue( val ).toString().toUtf8().data() ); \
+    return VariableValue::stringValue( qtTypeToUtf8Char( val ) ); \
 }
 
+#if QT_VERSION < 0x040000
+TRACELIB_SPECIALIZE_CONVERSION_USING_QVARIANT(QCString) // only in Qt3
+// QVariant does not serialize QStringList properly to a string
+template <> inline VariableValue convertVariable( QStringList val ) {
+    return VariableValue::stringValue( val.join(",").utf8() );
+}
+#else
+TRACELIB_SPECIALIZE_CONVERSION_USING_QVARIANT(QChar) // no QVariant overload in Qt3
+TRACELIB_SPECIALIZE_CONVERSION_USING_QVARIANT(QStringList)
+#endif
 TRACELIB_SPECIALIZE_CONVERSION_USING_QVARIANT(QString)
 TRACELIB_SPECIALIZE_CONVERSION_USING_QVARIANT(QByteArray)
-TRACELIB_SPECIALIZE_CONVERSION_USING_QVARIANT(QChar)
 TRACELIB_SPECIALIZE_CONVERSION_USING_QVARIANT(QDate)
 TRACELIB_SPECIALIZE_CONVERSION_USING_QVARIANT(QDateTime)
-TRACELIB_SPECIALIZE_CONVERSION_USING_QVARIANT(QStringList)
 TRACELIB_SPECIALIZE_CONVERSION_USING_QVARIANT(QTime)
 
+#undef qtTypeToUtf8Char
 #undef TRACELIB_SPECIALIZE_CONVERSION_USING_QVARIANT
 
 TRACELIB_NAMESPACE_END
