b2b41592ada273ef945ef3a84e1cb8ff156cee5d
Frerich Raabe <raabe@froglogic.com>
2014-02-24 15:28:50 +0100

    Blinde patch to fix Linux builds
    A recent commit made DebugViewErrorLog a Windows-only thing (as the name
    suggests).

--- tests/test_eventthreadunix.cpp
+++ tests/test_eventthreadunix.cpp
@@ -181,7 +181,7 @@ static void testCommunication()
     // as single fd in event thread
     testFileNotification();
 
-    DebugViewErrorLog error_log;
+    NullErrorLog error_log;
     std::string quick_fox = "The quick brown fox jumps over the lazy dog";
     NetworkOutput *net = new NetworkOutput( &error_log, "127.0.0.1", TRACELIB_DEFAULT_PORT );
     verify( "Initial NetworkOutput::canWrite()",
