627d50d42419448e61bc0f4fadc14b4626a93544
Frerich Raabe <raabe@froglogic.com>
2016-09-08 14:41:07 +0200

    Rename & Relicense source code under LGPL v3
    This renames the framework to 'tracetool', which is a little broader
    than 'tracelib' and which doesn't seem to be prone to trademark issues.
    The patch also relicenses all parts of the tracetool framework to be
    subject to the Lesser General Public License version 3.
    
    Prior versions of the framework were purely meant for internal usage,
    and froglogic reserved all rights to the software; the lack of an
    explicit license statement did not imply that the software was in the
    Public Domain.

--- gui/filterform.cpp
+++ gui/filterform.cpp
@@ -1,7 +1,21 @@
-/**********************************************************************
-** Copyright (C) 2010 froglogic GmbH.
-** All rights reserved.
-**********************************************************************/
+/* tracetool - a framework for tracing the execution of C++ programs
+ * Copyright 2010-2016 froglogic GmbH
+ *
+ * This file is part of tracetool.
+ *
+ * tracetool is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * tracetool is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with tracetool.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "filterform.h"
 
